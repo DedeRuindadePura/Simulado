0af3423806bf5705652d4546d1406690e922fbfc
DedeRuindadePura <familiamiranda1101@gmail.com>
2023-04-04 11:01:19 -0300

    Te amo calcio

---
+++ 04Xadrez.php
@@ -0,0 +1,9 @@
+<?php
+$L=fgets(STDIN);
+$C=fgets(STDIN);
+$A=$L=$C;
+
+if($A%2==0)
+{echo"1\n";} else
+{echo"0\n";}
+    ?>
---
+++ HOHOHO.php
@@ -0,0 +1,10 @@
+<?php
+$N = readline();
+for($a=1; $a<=$N; $a++)
+{echo "HO";
+if($a<$N)
+    {
+        echo " ";
+    }}
+echo "!";
+?>
